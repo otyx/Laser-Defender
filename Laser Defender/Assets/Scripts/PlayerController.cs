9585f7b4aa0dfd8513bd1529ea0fbb5767e36768
otyx <richard.andrews@otyx.com>
2016-09-26 21:32:03 +1000

    Complete Lecture 103
    Changelog:
    - add background.
    - add position limits by using Match.Clampf
    - implement movement using velocity of RigidBody2d rather than sprite position.

---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// speed modifier
+	[Range (0f, 1000f)]
+	public float shipSpeedFactor;
+
+	// the movement adjustment
+	private Vector2 delta = new Vector2();
+	private Vector2 STOP_VECTOR = new Vector2 ();
+
+	// the rigidbody for physics
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			// up arrow pressed
+			Debug.Log ("Pressed Up");
+			move (0, 1);
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			// down arrow pressed
+			Debug.Log ("Pressed Down");
+			move (0, -1);
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			// left arrow pressed
+			Debug.Log ("Pressed Left");
+			move (-1, 0);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			// Right arrow pressed
+			Debug.Log ("Pressed Right");
+			move (1, 0);
+		} else {
+			// no movement key pressed
+			Debug.Log("No Key pressed");
+			StopMovement ();
+		}
+
+		ClampPosition ();
+	}
+
+	private void move (int x, int y) {
+		delta.x = x * shipSpeedFactor * Time.deltaTime;
+		delta.y = y * shipSpeedFactor * Time.deltaTime;
+
+		rb.velocity = delta;
+	}
+
+	private void ClampPosition() {
+		rb.position = new Vector2 (Mathf.Clamp (rb.position.x, -6f, 6f), Mathf.Clamp (rb.position.y, -4.5f, 4.5f));
+	}
+
+	private void StopMovement() {
+		rb.velocity = STOP_VECTOR;
+	}
+}
