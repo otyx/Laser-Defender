89b037bd1713ebce41c68ca3dd994ad906778ea0
otyx <richard.andrews@otyx.com>
2016-09-27 20:00:59 +1000

    Complete Lecture 105
    Add enemy spawner
    Add Player prefab
    Set codebase to that of lecture, undo non-standard changes made in previous commit.

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	// the enemy prefab
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		GameObject enemy = Instantiate (enemyPrefab, new Vector3(), Quaternion.identity) as GameObject;
+		enemy.transform.parent = transform;
+		enemy.gameObject.name = "Enemy";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -4,60 +4,36 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	// speed modifier
-	[Range (0f, 1000f)]
-	public float shipSpeedFactor;
+	public float shipSpeedFactor = 15.0f;
+	public float padding;
 
-	// the movement adjustment
-	private Vector2 delta = new Vector2();
-	private Vector2 STOP_VECTOR = new Vector2 ();
+	// constraints of movement
+	float xmin = -5;
+	float xmax = 5;
 
-	// the rigidbody for physics
-	private Rigidbody2D rb;
-
-	// Use this for initialization
-	void Start () {
-		rb = gameObject.GetComponent<Rigidbody2D> ();
+	void Start() {
+		padding = GetComponent<SpriteRenderer> ().bounds.extents.x;
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			// up arrow pressed
-			Debug.Log ("Pressed Up");
-			move (0, 1);
-		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			// down arrow pressed
-			Debug.Log ("Pressed Down");
-			move (0, -1);
-		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			// left arrow pressed
 			Debug.Log ("Pressed Left");
-			move (-1, 0);
+			transform.position += Vector3.left * shipSpeedFactor * Time.deltaTime;
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			// Right arrow pressed
 			Debug.Log ("Pressed Right");
-			move (1, 0);
-		} else {
-			// no movement key pressed
-			Debug.Log("No Key pressed");
-			StopMovement ();
+			transform.position += Vector3.right * shipSpeedFactor * Time.deltaTime;
 		}
 
-		ClampPosition ();
-	}
-
-	private void move (int x, int y) {
-		delta.x = x * shipSpeedFactor * Time.deltaTime;
-		delta.y = y * shipSpeedFactor * Time.deltaTime;
-
-		rb.velocity = delta;
-	}
-
-	private void ClampPosition() {
-		rb.position = new Vector2 (Mathf.Clamp (rb.position.x, -6f, 6f), Mathf.Clamp (rb.position.y, -4.5f, 4.5f));
-	}
-
-	private void StopMovement() {
-		rb.velocity = STOP_VECTOR;
+		// restrict x movement
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 	}
 }
